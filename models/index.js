24c09880781d0a03af24858167f507b01d8c8167
Holly Parker <hollygcochran@gmail.com>
2022-10-24 17:01:12 -0500

    index js in models correction

--- models/index.js
+++ models/index.js
@@ -1,8 +1,5 @@
 const Users = require('./User');
 const Thoughts = require('./Thought');
-const { Router } = require('express');
 
-Router.use('/users', Users);
-Router.use('/thoughts', Thoughts);
 
 module.exports = { Users, Thoughts };
