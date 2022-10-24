462691485bbec96e768ae43e0b6654565c3b779f
Holly Parker <hollygcochran@gmail.com>
2022-10-24 16:49:32 -0500

    thought routes and assc thought controllers

---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router()
+// requiring all the methods from the thought controllers
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction,
+
+} = require ('../../controllers/thoughtsController')
+
+// getting all thoughts and posting on the / route
+router.route('/').get(getThoughts).post(createThought);
+// /:thoughtId getting a single thought by id, updating it with the body etc, and deleting it if needed
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+// posting a reaction to the route /:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+// deleting a reaction on the route :thoughtId/reactions/:reactionId reactions are assc with thoughts
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction)
