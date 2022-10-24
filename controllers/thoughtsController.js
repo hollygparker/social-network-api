c4c51cad6280878a36857752cbd2489764d222de
Holly Parker <hollygcochran@gmail.com>
2022-10-24 15:02:30 -0500

    getSingleThought

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -10,5 +10,18 @@ thoughtController = {
             console.log(err);
             res.status(500).json(err);
         });
+    },
+
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+        .then(async (thoughtDB) =>
+        !thoughtDB
+            ? res.status(404).json({ message: 'No thought found with that ID' })
+            : res.json({ thoughtDB })
+        )
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        });
     }
 }
