3712883310b60ac859680619e918cb24e1d6f3f8
Holly Parker <hollygcochran@gmail.com>
2022-10-24 12:53:33 -0500

    init fs

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const usersRoutes = require('./usersRoutes');
 const thoughtsRoutes = require('./thoughtsRoutes');
-const { builtinModules } = require('module');
 
 router.use('/users', usersRoutes);
 router.use('/thoughts', thoughtsRoutes);
