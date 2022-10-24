fef15ec1c3b82493ee069c352fc0bf00546a1bed
Holly Parker <hollygcochran@gmail.com>
2022-10-24 15:09:13 -0500

    createThought

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -23,5 +23,25 @@ thoughtController = {
             console.log(err);
             return res.status(500).json(err);
         });
+    },
+
+    createThought(req, res) {
+        Thought.create(req.body)
+        .then((thoughtDB) => {
+            return User.findOneAndUpdate(
+                { _id: req.body.userId },
+                { $addToSet: { thoughts: thoughtDB._id } },
+                { runValidators: true, new: true }
+            )
+        })
+
+        .then((userDB) =>
+            !userDB
+                ? res
+                    .status(404)
+                    .json({ message: 'No user found with that ID' })
+                : res.json(userDB)
+        )
+        .catch((err) => res.status(400).json(err));
     }
 }
